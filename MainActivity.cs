081a0358e099ce41defa73292cd7c9ac669bff2e
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2024-12-18 13:01:58 +0530

    Final Commit

---
+++ MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using System.Collections.Generic;
+
+namespace DotTask
+{
+    [Activity(Label = "DotTask", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private List<TaskItem> taskList;
+        private TaskAdapter taskAdapter;
+        private EditText taskInput;
+        private Button addTaskButton;
+        private ListView taskListView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            taskList = new List<TaskItem>();
+            taskAdapter = new TaskAdapter(this, taskList);
+
+            taskInput = FindViewById<EditText>(Resource.Id.taskInput);
+            addTaskButton = FindViewById<Button>(Resource.Id.addTaskButton);
+            taskListView = FindViewById<ListView>(Resource.Id.taskListView);
+
+            taskListView.Adapter = taskAdapter;
+
+            addTaskButton.Click += (sender, e) =>
+            {
+                var taskTitle = taskInput.Text;
+                if (!string.IsNullOrWhiteSpace(taskTitle))
+                {
+                    var newTask = new TaskItem(taskTitle);
+                    taskList.Add(newTask);
+                    taskAdapter.NotifyDataSetChanged();
+                    taskInput.Text = string.Empty;
+                }
+                else
+                {
+                    Toast.MakeText(this, "Task title cannot be empty", ToastLength.Short).Show();
+                }
+            };
+        }
+    }
+}
---
+++ TaskAdapter.cs
@@ -0,0 +1,42 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using System.Collections.Generic;
+
+namespace DotTask
+{
+    public class TaskAdapter : BaseAdapter<TaskItem>
+    {
+        private readonly Context context;
+        private readonly List<TaskItem> taskList;
+
+        public TaskAdapter(Context context, List<TaskItem> tasks)
+        {
+            this.context = context;
+            this.taskList = tasks;
+        }
+
+        public override TaskItem this[int position] => taskList[position];
+
+        public override int Count => taskList.Count;
+
+        public override long GetItemId(int position) => position;
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var item = taskList[position];
+            View view = convertView ?? LayoutInflater.From(context).Inflate(Android.Resource.Layout.SimpleListItemChecked, null);
+
+            // Bind TextView to Task Title
+            var textView = view.FindViewById<TextView>(Android.Resource.Id.Text1);
+            textView.Text = item.Title;
+
+            // Strike-through text if completed
+            textView.PaintFlags = item.IsCompleted
+                ? textView.PaintFlags | PaintFlags.StrikeThruText
+                : textView.PaintFlags & ~PaintFlags.StrikeThruText;
+
+            return view;
+        }
+    }
+}
